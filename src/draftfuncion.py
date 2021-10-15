a3a41ad3a413b8b45ff49767cb6c5175470e1c65
Maukrack <majiqtresi@gmail.com>
2021-10-15 17:12:34 -0500

    Adding draft function

---
+++ src/draftfuncion.py
@@ -0,0 +1,14 @@
+# Initial population
+#sigma  
+#mu
+#alpha
+
+#time, 1/250 days
+#v_performance_error, anual performance 
+#n_size, days
+
+# Define likelihood
+def likelihood(sigma, mu, alpha, time, v_performance_error, n_size):
+  #Todo: add formula
+  #result = ...
+  return result
